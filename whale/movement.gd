5f137494d0db5320aea864b9fa8794caef09ed42
JoãoMiguelVieiraCardoso <95939460+JoaoMiguelVieiraCardoso@users.noreply.github.com>
2025-01-24 22:43:28 +0000

    velocity up

---
+++ whale/movement.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 600.0
+var prev_direction = 1
+
+func _update_velocity(delta: float) -> void:
+	var h_direction := Input.get_axis("ui_left", "ui_right")
+	var v_direction := Input.get_axis("ui_up", "ui_down")
+	if h_direction:
+		velocity.x = h_direction * SPEED
+		if h_direction != prev_direction:
+			scale.x *= -1
+			prev_direction = h_direction
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if v_direction:
+		velocity.y = v_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	_update_velocity(delta)
+	move_and_slide()
