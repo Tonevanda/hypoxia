209aed7ea61043dca8af0a481671a869c8429e6c
JoãoMiguelVieiraCardoso <95939460+JoaoMiguelVieiraCardoso@users.noreply.github.com>
2025-01-24 23:04:34 +0000

    using flip instead of manual direction change

--- whale/movement.gd
+++ whale/movement.gd
@@ -1,17 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
+const SPEED = 300.0
 var prev_direction = 1
 
-func _update_velocity(delta: float) -> void:
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _flipH() -> void:
+	var h_direction := Input.get_axis("ui_left", "ui_right")
+	if h_direction > 0:
+		animated_sprite.flip_h = false
+	elif h_direction < 0:
+		animated_sprite.flip_h = true
+
+func _update_velocity() -> void:
 	var h_direction := Input.get_axis("ui_left", "ui_right")
 	var v_direction := Input.get_axis("ui_up", "ui_down")
 	if h_direction:
 		velocity.x = h_direction * SPEED
-		if h_direction != prev_direction:
-			scale.x *= -1
-			prev_direction = h_direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if v_direction:
@@ -24,5 +30,6 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	_update_velocity(delta)
+	_flipH()
+	_update_velocity()
 	move_and_slide()
