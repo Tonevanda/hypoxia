bb1c110c7760bc614dbcf28ea81b149e23e79c03
JoãoMiguelVieiraCardoso <95939460+JoaoMiguelVieiraCardoso@users.noreply.github.com>
2025-01-24 23:14:00 +0000

    fox idle

--- whale/movement.gd
+++ whale/movement.gd
@@ -24,12 +24,22 @@ func _update_velocity() -> void:
 		velocity.y = v_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+func _animate():
+	var h_direction := Input.get_axis("ui_left", "ui_right")
+	var v_direction := Input.get_axis("ui_up", "ui_down")
+	if h_direction == 0 and v_direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
 
 func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	_flipH()
 	_update_velocity()
 	move_and_slide()
+	_animate()
