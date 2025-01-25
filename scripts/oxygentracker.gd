47e06236abb599319170c19f168584c23076fce6
Tonevanda <up202108863@up.pt>
2025-01-25 16:19:53 +0000

    reorganized stuff

---
+++ scripts/oxygentracker.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var OXYGEN_LEVEL : float = 100.0
+var OXYGEN_DECREASE_RATE : float = 5.0
+
+func _process(delta: float) -> void:
+	# Decrease the oxygen level
+	OXYGEN_LEVEL -= OXYGEN_DECREASE_RATE * delta
+
+	# Ensure oxygen level doesn't go below 0
+	OXYGEN_LEVEL = max(OXYGEN_LEVEL, 0)
+
+	#Print the current oxygen level (for debugging)
+	print("OXYGEN_LEVEL: ", OXYGEN_LEVEL)
+
+	# Check if oxygen level reaches 0
+	if OXYGEN_LEVEL <= 0:
+		on_oxygen_depleted()
+
+func on_oxygen_depleted() -> void:
+	print("Oxygen depleted! Game over.")
+	
+	# TODO
+	#For now, go back to title screen, should show game over screen
+	get_tree().change_scene_to_file("res://scenes/titlescreen.tscn")
