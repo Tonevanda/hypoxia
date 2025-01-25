db3085f915c1b232f185d1f5cd26896cf242a30b
JoãoMiguelVieiraCardoso <95939460+JoaoMiguelVieiraCardoso@users.noreply.github.com>
2025-01-25 20:25:15 +0000

    collision entities are being added to arrays instead of just checking boolean

--- scripts/straight.gd
+++ scripts/straight.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
-var isColliding = false
+var collidingEntities = []
+var direction = 1
 const SPEED = 10.0
 
 
+func set_direction(casterDirection: int) -> void:
+	direction = casterDirection
+
+func handleCollisions():
+	collidingEntities = []
+	queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if isColliding:
-		queue_free()
+	if !collidingEntities.is_empty():
+		handleCollisions()
 	else:
-		position.x += SPEED * delta
+		position.x += direction * SPEED * delta
+	
--- scripts/towardsPlayerMovement.gd
+++ scripts/towardsPlayerMovement.gd
@@ -3,13 +3,20 @@ extends Node2D
 
 
 @onready var player = get_node("/root/Game/Player/Whale")
-var isColliding = false
+var collidingEntities = []
 const SPEED = 10.0
 
-
+func handleCollisions(delta: float):
+	print(collidingEntities)
+	for entity in collidingEntities:
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !isColliding:
+	if !collidingEntities.is_empty():
+		print("colliding")
+		handleCollisions(delta)
+	else:
 		position.x = move_toward(position.x, player.position.x, SPEED*delta)
 		position.y = move_toward(position.y, player.position.y, SPEED*delta)
+	collidingEntities = []
